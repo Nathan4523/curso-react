0827ee96e555e38208e28db9d6bbeb943054cb74
Nicolas Nathan Campos <camposnicolas45@gmail.com>
2020-08-29 17:28:56 -0300

    reactjs do bootcamp

---
+++ react-bootcamp-rocket/babel.config.js
@@ -0,0 +1,10 @@
+//min config do babel
+module.exports = {
+    presets: [
+        '@babel/preset-env',
+        '@babel/preset-react'
+    ],
+    plugins: [
+        '@babel/plugin-transform-runtime'
+    ]
+}
---
+++ react-bootcamp-rocket/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+import App from './App';
+
+//JSX: HTML dentro do JavaScript
+// render(<h1>Hello World</h1>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
